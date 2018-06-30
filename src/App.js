0baa5817da3a6de82d5aa342b22ad792b12daa6a
icyJoseph <sephxd1234@gmail.com>
2018-06-30 23:19:53 +0200

    feat: render smartphone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { AllLogos } from "./logos";
+import Smartphone from "./logos/Smartphone.png";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Weather App</h1>
-        </header>
-        {AllLogos.map((Logo, index) => (
-          <svg key={index} style={{ height: "300px", margin: 100 }}>
-            <Logo />
-          </svg>
-        ))}
-      </div>
-    );
+    return <img alt="smartphone" src={Smartphone} />;
   }
 }
 
