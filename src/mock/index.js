5523608488fe0e9a60e9b2e8758f8b17a419f4cc
icyJoseph <sephxd1234@gmail.com>
2018-07-01 00:05:04 +0200

    feat: mock data from server call

---
+++ src/mock/index.js
@@ -0,0 +1,42 @@
+export default {
+  coord: {
+    lon: 11.97,
+    lat: 57.71
+  },
+  weather: [
+    {
+      id: 800,
+      main: "Clear",
+      description: "clear sky",
+      icon: "01d"
+    }
+  ],
+  base: "stations",
+  main: {
+    temp: 293.15,
+    pressure: 1020,
+    humidity: 30,
+    temp_min: 293.15,
+    temp_max: 293.15
+  },
+  visibility: 10000,
+  wind: {
+    speed: 6.2,
+    deg: 20
+  },
+  clouds: {
+    all: 0
+  },
+  dt: 1530287400,
+  sys: {
+    type: 1,
+    id: 5384,
+    message: 0.0266,
+    country: "SE",
+    sunrise: 1530238527,
+    sunset: 1530303331
+  },
+  id: 2711537,
+  name: "Gothenburg",
+  cod: 200
+};
