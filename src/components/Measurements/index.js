14b7b15e4975994402c0b10c779f10b9c371fff2
icyJoseph <sephxd1234@gmail.com>
2018-07-01 00:04:07 +0200

    refactor: single file component

---
+++ src/components/Measurements/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Measurements = () => (
+  <div
+    style={{
+      flex: 3,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      color: "white"
+    }}
+  >
+    Measurements
+  </div>
+);
