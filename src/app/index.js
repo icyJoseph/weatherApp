1d20c6b4680c4d8bcce4ecda301588f99c40ff60
icyJoseph <sephxd1234@gmail.com>
2018-07-01 01:17:50 +0200

    feat: break down mock data through sequence and pass it to children

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import { Measurements } from "../components/Measurements";
 import { Weather } from "../components/Weather";
-
 import { Smartphone } from "../components/Smartphone";
 
 import { Container } from "../components/Styled/Container";
 
+import { sequence } from "./utils";
+
+import Mock from "../mock";
+
 class App extends Component {
+  state = { measurements: null, indicators: null };
+
+  componentDidMount() {
+    // Later on replace Promise.resolve(Mock) with axios get
+
+    return Promise.resolve(Mock)
+      .then(sequence)
+      .then(({ measurements, indicators }) =>
+        this.setState({ measurements, indicators })
+      );
+  }
   render() {
     return (
       <Container>
-        <Measurements />
-        <Weather />
+        <Measurements measurements={this.state.measurements} />
+        <Weather indicators={this.state.indicators} />
         <Smartphone />
       </Container>
     );
