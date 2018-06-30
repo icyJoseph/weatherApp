a5e5b4b72817870375acebb339524ff3334c7e5f
icyJoseph <sephxd1234@gmail.com>
2018-07-01 01:51:43 +0200

    feat: show temp, wind and description

--- src/components/Measurements/index.js
+++ src/components/Measurements/index.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { FlexSection } from "../Styled/FlexSection";
 
-export const Measurements = ({ measurements }) => {
+const Description = ({ description = "" }) => (
+  <FlexSection>{description}</FlexSection>
+);
+const Temp = ({ temp = 0 }) => <FlexSection>{temp} K</FlexSection>;
+const Wind = ({ wind = { speed: 0 } }) => (
+  <FlexSection>{wind.speed} m/s</FlexSection>
+);
+
+const Loading = () => <div>Loading...</div>;
+
+export const Measurements = ({ measurements = {} }) => {
+  const { description, temp, wind } = measurements;
   return (
     <FlexSection
       flex={3}
-      justifyContent="center"
-      alignItems="center"
-      color="white"
+      direction="column"
+      style={{
+        marginTop: 40
+      }}
     >
-      {measurements ? <div>Measurements</div> : <div>Loading...</div>}
+      {measurements ? (
+        <Fragment>
+          <Description description={description} />
+          <Temp temp={temp} />
+          <Wind wind={wind} />
+        </Fragment>
+      ) : (
+        <Loading />
+      )}
     </FlexSection>
   );
 };
