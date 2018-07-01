a2d4744b4ba6bebd300f8227c9b87ce76f328fa8
icyJoseph <sephxd1234@gmail.com>
2018-07-01 02:07:06 +0200

    refactor: use styled svg SvgContainer

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Sun } from "../../logos/sun";
 import { FlexSection } from "../Styled/FlexSection";
-
+import { SvgContainer } from "../Styled/SvgContainer";
 export const Weather = ({ indicators }) => {
-  console.log(indicators);
   return (
-    <FlexSection flex={3}>
-      <svg width="100" height="100" viewBox="0 0 300 300">
+    <FlexSection flex={2} alignItems="flex-end">
+      <SvgContainer viewBox="0 0 300 300">
         <Sun />
-      </svg>
+      </SvgContainer>
     </FlexSection>
   );
 };
