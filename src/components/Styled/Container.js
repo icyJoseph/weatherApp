ebb797f3bfacc50aaf0eceff8bf97f2fdec875ad
icyJoseph <sephxd1234@gmail.com>
2018-07-01 21:12:43 +0200

    style: round corners, align content and white background

--- src/components/Styled/Container.js
+++ src/components/Styled/Container.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100vh;
+  background: white;
   display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
+  border-radius: 40px;
 `;
