790cef2c54ddb3aa2fac9283042fc88411884eea
icyJoseph <sephxd1234@gmail.com>
2018-07-01 21:09:35 +0200

    refactor: use default values

--- src/components/Styled/SvgContainer.js
+++ src/components/Styled/SvgContainer.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const SvgContainer = styled.svg`
-  width: 100px;
-  height: 100px;
+  width: ${props => props.width || "100px"};
+  height: ${props => props.height || "100px"};
 `;
