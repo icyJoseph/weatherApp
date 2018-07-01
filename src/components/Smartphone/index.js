d0516dbc2c107d285769bc6ce761e822fbf247c5
icyJoseph <sephxd1234@gmail.com>
2018-07-01 21:09:01 +0200

    feat: apply round borders

--- src/components/Smartphone/index.js
+++ src/components/Smartphone/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { FlexSection } from "../Styled/FlexSection";
-
+import { Image } from "../Styled/Image";
 import smartphone from "../../logos/smartphone.png";
 
 export const Smartphone = () => (
-  <FlexSection flex={5} alignItems="flex-start">
-    <img style={{ width: "500px" }} alt="smartphone" src={smartphone} />
+  <FlexSection
+    flex={4}
+    style={{
+      borderRadius: "300px 300px 40px 40px/ 300px 300px 40px 40px",
+      background: "#fc6363"
+    }}
+  >
+    <Image alt="smartphone" src={smartphone} />
   </FlexSection>
 );
