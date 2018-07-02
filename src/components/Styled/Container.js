e61f05e5e6ff619a546ba2626356ca52c1d70074
icyJoseph <sephxd1234@gmail.com>
2018-07-02 08:03:53 +0200

    feat: darker background and box shadow

--- src/components/Styled/Container.js
+++ src/components/Styled/Container.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: white;
+  background: #f5f5f5;
   display: flex;
   flex: 1;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   border-radius: 40px;
+  box-shadow: 2px 2px 5px 4px rgba(53, 53, 53, 0.95);
 `;
