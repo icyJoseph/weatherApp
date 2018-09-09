eef8006cdf9abcd4ac0d00f69b29ef7491bb1207
icyJoseph <sephxd1234@gmail.com>
2018-09-09 13:51:57 +0200

    feat: Component for all possible icon names

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { FlexSection, SvgContainer } from "../Styled";
 
+import { Moon } from "../../logos/moon";
 import { Sun } from "../../logos/sun";
 import { Snowflake } from "../../logos/snowflake";
 import { Cloudy } from "../../logos/cloudy";
 import { Lightning } from "../../logos/lightning";
 import { Rainy } from "../../logos/rainy";
+import { WindyDay } from "../../logos/windyDay";
 
 const selector = icon => {
   switch (icon) {
-    case "clear":
+    case "clear-day":
       return Sun;
-    case "clouds":
+    case "clear-night":
+      return Moon;
+    case "wind":
+      return WindyDay;
+    case "fog":
+    case "cloudy":
+    case "partly-cloudy-day":
+    case "partly-cloudy-night":
       return Cloudy;
     case "rain":
       return Rainy;
     case "snow":
+    case "sleet":
       return Snowflake;
     case "thunderstorm":
       return Lightning;
@@ -33,7 +43,8 @@ export const Weather = ({ currently: { icon = "" } }) => {
       style={{
         background: "#fc6363",
         borderRadius: "50% 50% 40px 40px",
-        width: "100%"
+        width: "96%",
+        padding: "2%"
       }}
     >
       <SvgContainer width="100px" height="100px" viewBox="0 0 300 300">
