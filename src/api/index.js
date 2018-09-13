34f00b616614f04771508fa4684a31f75d1b42a5
icyJoseph <sephxd1234@gmail.com>
2018-09-13 22:47:47 +0200

    chore: update api endpoints to byAddress and byLatLng

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
-const endPoint =
-  "https://wt-eb857ce9bac13bdd2efcf8e93fb9fdcc-0.sandbox.auth0-extend.com/weather-express/geoweather";
+const byAddressEndPoint =
+  "https://wt-eb857ce9bac13bdd2efcf8e93fb9fdcc-0.sandbox.auth0-extend.com/weather-express/byAddress";
+const byLatLngEndPoint =
+  "https://wt-eb857ce9bac13bdd2efcf8e93fb9fdcc-0.sandbox.auth0-extend.com/weather-express/byLatLng";
 
 export const addExpiry = weather => {
   const now = new Date().getTime();
   return { ...weather, expiry: now + 1000 };
 };
 
-export const weatherPipe = (query, cb, errCb) => {
+export const byAddressPipe = (query, cb, errCb) => {
   return axios
-    .post(endPoint, { address: query })
+    .post(byAddressEndPoint, { address: query })
     .then(({ data }) => data)
     .then(addExpiry)
     .then(res => cb(res, query))
