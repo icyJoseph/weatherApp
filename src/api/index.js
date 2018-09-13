89781d0a39f21dc0115741bee122c06fba11c6fe
icyJoseph <sephxd1234@gmail.com>
2018-09-13 21:10:28 +0200

    chore: update to geoweather endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const endPoint =
-  "https://wt-eb857ce9bac13bdd2efcf8e93fb9fdcc-0.sandbox.auth0-extend.com/weather-express/geo";
+  "https://wt-eb857ce9bac13bdd2efcf8e93fb9fdcc-0.sandbox.auth0-extend.com/weather-express/geoweather";
 
 export const addExpiry = weather => {
   const now = new Date().getTime();
