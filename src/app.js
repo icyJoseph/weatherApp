84b022f8caad12c66abf9e6add8f905db808fedd
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-04-06 13:59:45 +0200

    refactor: rewrite with hooks

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useReducer, useEffect } from "react";
 import { Geography } from "./components/Geography";
 import { Emoji } from "./components/Emoji";
 import { Measurements } from "./components/Measurements";
 import { Loading } from "./components/Loading";
 import { Search } from "./components/Search";
 import { Weather } from "./components/Weather";
-import { Magic } from "./components/MagicButton";
-import { WeatherCard, Container, EmojiContainer } from "./components/Styled";
+import { Magic } from "./components/Magic";
+import { Clear } from "./components/Clear";
+import { Refresh } from "./components/Refresh";
+import {
+  WeatherCard,
+  Container,
+  EmojiContainer,
+  Bottom,
+  Results
+} from "./components/Styled";
 import { byAddressPipe, byLatLngPipe } from "./api";
 import {
   getHistory,
@@ -17,142 +25,126 @@ import {
 } from "./utils";
 import { debounce } from "./helpers";
 import { weatherApp, FailureEmojis } from "./constants";
+import reducer, {
+  initialState,
+  SET_PERMISSION,
+  SET_LOCATION,
+  SET_RESULTS,
+  ADD_RESULT,
+  SET_LOADING,
+  SET_ERROR,
+  SET_QUERY
+} from "./reducer";
 
-class App extends Component {
-  state = {
-    query: "",
-    weather: null,
-    error: null,
-    history: [],
-    loading: false,
-    permission: "",
-    lat: null,
-    lng: null
-  };
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  componentDidMount() {
-    // if no history, then it remains at []
-    return (
-      checkGeoLocationPermission(this.setGeoLocationPermission) ||
-      getHistory(weatherApp, this.setHistory)
-    );
-  }
+  const { query, error, loading, lat, lng, results } = state;
 
-  setGeoLocationPermission = state => this.setState({ permission: state });
-
-  setLocationCoordinates = (lat, lng) => this.setState({ lat, lng });
-
-  getGeoLocation = () =>
-    reportGeoLocation(this.setLocationCoordinates, this.setError).then(() => {
-      const { lat, lng } = this.state;
-      this.setLoading();
-      return this.fetchWeatherByLatLng(
-        { lat, lng },
-        this.updateState,
-        this.setError
-      );
-    });
-
-  // update state after a search
-  updateState = (weather, query) => {
-    const toSave = { ...weather, query };
-    const history = getHistory(weatherApp);
-    const updatedHistory = updateHistory(weatherApp, history, toSave);
-    return this.setState(prevState => ({
-      ...prevState,
-      history: updatedHistory,
-      weather: toSave,
-      loading: false,
-      error: null
-    }));
-  };
-
-  // setters
-  setWeather = weather => {
-    return this.setState(prevState => ({
-      ...prevState,
-      weather,
-      error: null,
-      loading: false
-    }));
-  };
-
-  setHistory = history => {
-    return this.setState(prevState => ({
-      ...prevState,
-      history,
-      error: null
-    }));
-  };
-
-  setError = error => this.setState({ error, loading: false });
-  setLoading = () => this.setState({ loading: true });
-
-  // handlers
-  handleChange = event => {
-    return this.setState({
-      query: event.target.value.toLowerCase()
+  // error action creator
+  const setError = payload => dispatch({ type: SET_ERROR, payload });
+  // loading action creator
+  const setLoading = () => dispatch({ type: SET_LOADING });
+  // dispatch set location
+  const setLocationCoordinates = (lat, lng) =>
+    dispatch({ type: SET_LOCATION, payload: { lat, lng } });
+  // call for reportGeoLocation, browser query
+  const getGeoLocation = () =>
+    reportGeoLocation(setLocationCoordinates, setError);
+  // pair a query with weather data and dispatch
+  const addResult = (weather, query) => {
+    const payload = { ...weather, query };
+    return dispatch({
+      type: ADD_RESULT,
+      payload
     });
   };
-
-  handleSubmit = event => {
+  // update query on reducer
+  const handleChange = event => {
+    return dispatch({
+      type: SET_QUERY,
+      payload: event.target.value.toLowerCase()
+    });
+  };
+  // handle query submission
+  const handleSubmit = event => {
     event.preventDefault();
 
-    const { query } = this.state;
+    if (loading) {
+      return null;
+    }
 
-    if (query === "") return null;
-    this.setLoading();
-    const history = getHistory(weatherApp);
-    const existingValidWeatherData = existingValidData(history, query);
+    if (query === "") {
+      return null;
+    }
 
-    if (existingValidWeatherData)
-      return this.setWeather(existingValidWeatherData);
+    const storedResults = getHistory(weatherApp);
+    const existingValidWeatherData = existingValidData(storedResults, query);
 
-    return this.fetchWeatherByAddress(
-      query,
-      this.updateState,
-      this.setError,
-      this.setLoading
-    );
+    if (existingValidWeatherData) {
+      return addResult(existingValidWeatherData, query);
+    }
+
+    return fetchWeatherByAddress(query, addResult, setError, setLoading);
   };
 
-  // debounce fetch
-  fetchWeatherByAddress = debounce(byAddressPipe, 1000);
-  fetchWeatherByLatLng = debounce(byLatLngPipe, 1000);
+  // debounce fetch api calls
+  const fetchWeatherByAddress = debounce(byAddressPipe, 1000);
+  const fetchWeatherByLatLng = debounce(byLatLngPipe, 1000);
 
-  render() {
-    const { weather, query, error, loading } = this.state;
-    const shouldShowFailureEmoji = error && !loading;
-    const shouldShowMagicWand = !weather && !error && !loading;
-    const shouldShowWeatherCard = weather && !error && !loading;
-    return (
-      <Fragment>
-        <Search
-          query={query}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-        <Container>
-          {shouldShowFailureEmoji && (
-            <EmojiContainer>
-              {FailureEmojis.map(({ label, symbol }) => (
-                <Emoji key={label} label={label} symbol={symbol} />
-              ))}
-            </EmojiContainer>
-          )}
-          {shouldShowMagicWand && <Magic onClick={this.getGeoLocation} />}
-          {loading && <Loading type="balls" color="white" />}
-          {shouldShowWeatherCard && (
-            <WeatherCard>
-              <Geography {...weather} />
-              <Measurements {...weather} />
-              <Weather {...weather} />
-            </WeatherCard>
-          )}
-        </Container>
-      </Fragment>
+  useEffect(() => {
+    checkGeoLocationPermission(payload =>
+      dispatch({ type: SET_PERMISSION, payload })
     );
-  }
+
+    getHistory(weatherApp, payload => dispatch({ type: SET_RESULTS, payload }));
+  }, []);
+
+  useEffect(() => {
+    lat &&
+      lng &&
+      fetchWeatherByLatLng({ lat, lng }, addResult, setError, setLoading);
+  }, [lat, lng]);
+
+  useEffect(() => {
+    updateHistory(weatherApp, results);
+  }, [results]);
+
+  return (
+    <Fragment>
+      <Search
+        query={query}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        disabled={loading}
+      />
+      <Container>
+        {error && (
+          <EmojiContainer>
+            {FailureEmojis.map(({ label, symbol }) => (
+              <Emoji key={label} label={label} symbol={symbol} />
+            ))}
+          </EmojiContainer>
+        )}
+        {loading && <Loading type="spin" color="#663633" />}
+        <Results>
+          {results.map(data => (
+            <WeatherCard key={data.query}>
+              <Geography {...data} />
+              <Measurements {...data}>
+                <Weather {...data} />
+              </Measurements>
+            </WeatherCard>
+          ))}
+        </Results>
+      </Container>
+      <Bottom>
+        <Magic callback={getGeoLocation} />
+        <Refresh callback={() => {}} />
+        <Clear callback={() => dispatch({ type: SET_RESULTS, payload: [] })} />
+      </Bottom>
+    </Fragment>
+  );
 }
 
 export default App;
