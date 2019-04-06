a670413b7027d0323bf45064e303411c1b719006
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-04-06 14:01:38 +0200

    style: simplify markup

--- src/components/Measurements/index.js
+++ src/components/Measurements/index.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { FlexSection, SvgContainer } from "../Styled";
 import { Thermometer } from "../Thermometer";
 
-export const Measurements = ({ currently = {}, daily: { data = [] } }) => {
+export const Measurements = ({
+  currently = {},
+  daily: { data = [] },
+  children
+}) => {
   const { temperature } = currently;
 
   const [{ temperatureHigh, temperatureLow }] = data;
@@ -13,23 +16,26 @@ export const Measurements = ({ currently = {}, daily: { data = [] } }) => {
   ];
 
   return (
-    <FlexSection flex={3} style={{ margin: "auto 20px" }}>
-      <SvgContainer width="100px" height="100px" viewBox="0 0 300 300">
-        <Thermometer celsius={celsius} />
-      </SvgContainer>
-      <FlexSection direction="column">
-        <span style={{ fontSize: 48 }}>
-          {celsius.toFixed(2)}
+    <div>
+      <span>
+        {children}
+        <svg width="50px" height="50px" viewBox="0 0 300 300">
+          <Thermometer celsius={celsius} />
+        </svg>
+        <span style={{ fontSize: "2em" }}>
+          {celsius.toFixed(0)}
           <span>&deg;C</span>
         </span>
-        <span style={{ fontSize: 16 }}>
+      </span>
+      <div>
+        <span style={{ fontSize: "1em" }}>
           min: {minTemp.toFixed(0)}
           <span>&deg;C</span>
           {" - "}
           max: {maxTemp.toFixed(0)}
           <span>&deg;C</span>
         </span>
-      </FlexSection>
-    </FlexSection>
+      </div>
+    </div>
   );
 };
